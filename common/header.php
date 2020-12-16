6f8cfdaf7c4083129fcef89912ea9ac7b7ecf1dc
rubenmantecon <cuentas.mantecon@gmail.com>
2020-12-16 15:38:50 +0100

    Scaffolded header (WIP)

---
+++ common/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tricount</title>
+</head>
+<header>
+	<div>
+		<ul>
+			<li>
+				<?php echo $_POST['logged_user'] //Or whatever?>
+			</li>
+			<li>
+				<a href="index.php">HOME- Ojito que la ruta puede estar mal; __DIR__, etc</a>
+			</li>
+		</ul>
+	</div>
+</header>
