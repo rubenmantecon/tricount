801b4859fef3f272d12293ef2341952b7a02f597
Sergio Soffiati <sergiogarciasoffiati@gmail.com>
2020-12-16 14:08:57 +0100

    Function createForm

--- function/j.js
+++ function/j.js
@@ -10,4 +10,30 @@ function messageWarning (number,message){
     else{
         document.getElementById("warningSpace").innerHTML=('<div style="background-color:#ebf81e; width:50%;"><h5>Warning:'+ msgInWarning +'</h5></div>')
     }    
+    f.setAttribute('method',"post");
+    f.setAttribute('action',"submit.php");
+
+    var i = document.createElement("input"); //input element, text
+    i.setAttribute('type',"text");
+    i.setAttribute('name',"name");
+    i.setAttribute('placeholder',"Nombre");
+
+    var o = document.createElement("input"); 
+    o.setAttribute('type',"text");
+    o.setAttribute('name',"description");
+    o.setAttribute('placeholder',"Descripción");
+    var p = document.createElement("input"); 
+    p.setAttribute('type',"text");
+    p.setAttribute('name',"moneyType");
+    p.setAttribute('placeholder',"Moneda");
+
+    var s = document.createElement("input"); //input element, Submit button
+    s.setAttribute('type',"submit");
+    s.setAttribute('value',"Enviar");
+
+    f.appendChild(i);
+    f.appendChild(o);
+    f.appendChild(p);
+    f.appendChild(s);
+    document.getElementsByTagName('body')[0].appendChild(f);
 }
