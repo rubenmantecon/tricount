39052cdfa4ca6adcde458cc005d725ddf856f212
byrambo16 <57258946+byrambo16@users.noreply.github.com>
2020-12-17 12:18:37 +0100

    [ADD] Header footer y mensages de error y succes

--- common/home.php
+++ common/home.php
@@ -2,22 +2,27 @@
     declare(strict_types=1);
     include '../function/p.php';
     $dbconection = connectToDatabase();
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/home.css">
+    <link rel="stylesheet" href="../style/main.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
 <body>
-    <header>
-        <link rel="stylesheet" href="../style/home.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    </header>
-    <div>
+
+    <?php require("header.php");?>
+    <div class="minheight">
+        <div id="warningSpace" class="d-flex justify-content-center buttons text-center">
+
+        </div>
         <div class="d-flex justify-content-center buttons">
             <button type="button" class="btn btn-primary btn-lg sortCreate" disabled="true">Por fecha de creacion</a>
             <button type="button" class="btn btn-primary btn-lg sortUpdate">Por fecha de actualizacion</a>
@@ -68,9 +73,12 @@
         </table>
         </div>
         <div class="d-flex justify-content-center">
-            <a href="" type="button" class="btn btn-secondary btn-lg">Añadir viaje</a>
+            <a href="../src/invitations.html" type="button" class="btn btn-secondary btn-lg">Añadir viaje</a>
         </div>
     </div>
+    <script src="../function/j.js"></script>
     <script src="../function/home.js"></script>
+    <?php require("footer.html")?>
 </body>
+
 </html>
