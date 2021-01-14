5cb8c922ccb012eb8a220ee26dcd5905c3fce2e7
aexgomezdev <byrambo166@gmail.com>
2021-01-14 23:18:03 +0100

    Register sanitizado

--- common/login.php
+++ common/login.php
@@ -24,9 +24,11 @@ declare(strict_types=1); ?>
 	<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 
 </head>
+
 <?php
+//Sanitizacion de los campos del loguin
 session_start();
-$error = array();
+$errorLog = array();
 include '../function/p.php';
 $sanitizedData = [];
 $dbconection = connectToDatabase();
@@ -34,12 +36,12 @@ if (isset($_POST['logearse'])) {
 	// Requerimos el email también:
 	if (empty($_POST["password"])) {
 		//Codigo sergio
-		array_push($error, "Tienes que insertar una contraseñaa");
+		array_push($errorLog, "Tienes que insertar una contraseñaa");
 	} else {
 		$sanitizedData['password'] = $_POST['password'];
 	}
 	if (empty($_POST["emailog"])) {
-		array_push($error, "Tienes que insertar un correo");
+		array_push($errorLog, "Tienes que insertar un correo");
 	} else {
 		// Queremos que el email tenga un formato adecuado
 		if (filter_var($_POST['emailog'], FILTER_VALIDATE_EMAIL)) {
@@ -52,13 +54,13 @@ if (isset($_POST['logearse'])) {
 						$passwordQuery->execute();
 						$passwordQuery = $passwordQuery->fetchAll();
 						if ($passwordQuery[0]["password"] == $sanitizedData["password"]) {
-							$error = false;
+							$errorLog = false;
 							$_SESSION['username'] = $passwordQuery[0]["username"];
 							header("Location: home.php");
 							break;
 						} else {
 							//Codigo sergio
-							array_push($error, "Contraseña incorrecta");
+							array_push($errorLog, "Contraseña incorrecta");
 							break;
 						}
 					}
@@ -66,7 +68,69 @@ if (isset($_POST['logearse'])) {
 			}
 		} else {
 			//Codigo sergio
-			array_push($error, "Correo no valido");
+			array_push($errorLog, "Correo no valido");
+		}
+	}
+}
+?>
+<?php
+//Sanitiizacion del register
+$sanitizedDataReg = [];
+$errorReg = array();
+if (isset($_POST['registrarse'])) {
+	// Requerimos el email también:
+	if (empty($_POST["passwordreg"])) {
+		//Codigo sergio
+		array_push($errorReg, "Tienes que insertar una contraseñaa");
+	} else {
+		if (empty($_POST["passwordreg2"])) {
+			array_push($errorReg, "Las contraseñas no coinciden");
+		} else {
+			if ($_POST["passwordreg"] = $_POST["passwordreg2"]) {
+				$sanitizedDataReg['passwordreg'] = $_POST['passwordreg'];
+			}
+		}
+	}
+
+	if (empty($_POST['name'])) {
+		array_push($errorReg, "Tienes que insertar un nombre");
+	} else {
+		$sanitizedDataReg['name'] = $_POST['name'];
+	}
+	if (empty($_POST['surname'])) {
+		array_push($errorReg, "Tienes que insertar un apellido");
+	} else {
+		$sanitizedDataReg['surname'] = $_POST['surname'];
+	}
+	if (empty($_POST['username'])) {
+		array_push($errorReg, "Tienes que insertar un nombre de usuario");
+	} else {
+		$sanitizedDataReg['username'] = $_POST['username'];
+	}
+	if (empty($_POST["emailreg"])) {
+		array_push($errorReg, "Tienes que insertar un correo");
+	} else {
+		// Queremos que el email tenga un formato adecuado
+		if (filter_var($_POST['emailreg'], FILTER_VALIDATE_EMAIL)) {
+			$sanitizedDataReg['emailreg'] = $_POST['emaire'];
+			$emailsqueryreg = executeSelect2("USERS", "email");
+			foreach ($emailsqueryreg as $debug) {
+				foreach ($debug as $debug2) {
+					if ($debug2 == $sanitizedData['emailreg']) {
+						array_push($errorReg, "Ese correo ya existe");
+					} else {
+						$userInsert = $dbconection->prepare('INSERT INTO 
+						users(name,surname,email,username,password)
+					VALUES
+						("' . $sanitizedDataReg['name'] . '","' . $sanitizedDataReg['surname'] . '","' . $sanitizedDataReg['emailreg'] . '","' . $sanitizedDataReg['username'] . '","' . $sanitizedDataReg['passwordreg'] . '");');
+						$userInsert->execute();
+						$userInsert = $userInsert->fetchAll();
+					}
+				};
+			}
+		} else {
+			//Codigo sergio
+			array_push($errorLog, "Correo no valido");
 		}
 	}
 }
@@ -84,9 +148,12 @@ if (isset($_POST['logearse'])) {
 		<div class="thumbnail"><img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Tux_Mono.svg" /></div>
 		<form class="register-form">
 			<input type="text" placeholder="name" name="name" />
+			<input type="text" placeholder="surname" name="surname" />
+			<input type="text" placeholder="username" name="username" />
 			<input type="password" placeholder="password" name="passwordreg" id="passwordreg" />
+			<input type="password" placeholder="password" name="passwordreg2" id="passwordreg2" />
 			<input type="text" placeholder="email address" name="emailreg" id="emailreg" />
-			<button>create</button>
+			<button type="submit" nanme="registrarse">create</button>
 			<p class="message">Ya estas registrado? <a href="#">Sign In</a></p>
 		</form>
 		<form class="login-form" action="login.php" method="post">
@@ -95,7 +162,7 @@ if (isset($_POST['logearse'])) {
 			<input type="submit" name="logearse"></button>
 			<?php
 			echo "<div class='errorColor'>";
-			foreach ($error as $x) {
+			foreach ($errorLog as $x) {
 				echo "<p>" . $x . "</p>";
 			}
 			echo "</div>";
