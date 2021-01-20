c1bcd1a69fc5b12000cf26f9fd09e13d4daa7b33
rubenmantecon <cuentas.mantecon@gmail.com>
2021-01-20 20:24:31 +0100

    Offer wrong emalis for correction

--- common/invitations.php
+++ common/invitations.php
@@ -1,28 +1,44 @@
+<?php 
+declare(strict_types=1);
+session_start();
+include('../function/p.php');?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-	<script type="text/javascript"src="../function/j.js"></script>
+	<script type="text/javascript" src="../function/j.js"></script>
 	<title>Invitaciones</title>
 </head>
-<?php session_start();
-include('../function/p.php');
+<?php 
 $correctlyFormattedEmails = [];
+$incorrectlyFormattedEmails = [];
 if (isset($_POST['invite'])) {
 	foreach ($_POST['email'] as $email) {
 		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			$correctlyFormattedEmails[] = $email;
 		} else {
-			echo '<script>newEmailField();</script>';
-			echo "\n" . $email . " is not a valid email";
+			$incorrectlyFormattedEmails[] = $email;
 		}
 	}
-	print_r($correctlyFormattedEmails);
-	$classifiedEmails = classifyEmails($correctlyFormattedEmails);
-	processEmailInvitations($classifiedEmails['inDB']);
-	processEmailInvitations($classifiedEmails['notInDB']);
-
 }
+
+if (!empty($incorrectlyFormattedEmails)) {
+	foreach ($incorrectlyFormattedEmails as $email => $value) {
+		if (!empty($value)) {
+			echo '<p>These emails are incorrect</p>';
+			echo '<form method="post" id="emailForm">';
+			echo "<input type=\"email\" value=\"{$value}\" name=\"email[]\">";
+		} 
+		
+	}
+}
+$classifiedEmails = classifyEmails($correctlyFormattedEmails);
+processEmailInvitations($classifiedEmails['inDB']);
+processEmailInvitations($classifiedEmails['notInDB']);
+
 ?>
+
 <body>
 	<center>
 		<form method="post" id="emailForm">
@@ -34,4 +50,5 @@ if (isset($_POST['invite'])) {
 		</div>
 	</center>
 </body>
+
 </html>
